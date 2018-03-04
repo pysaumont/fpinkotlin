54dc862319a77f389af4084da69471d201ecec76
pysaumont <pys@volga.fr>
2018-03-04 21:56:28 +0100

    Edit to chapter 4

--- fpinkotlin-parent/fpinkotlin-functions-solutions/src/test/kotlin/com/fpinkotlin/functions/exercise04/FunctionsTest.kt
+++ fpinkotlin-parent/fpinkotlin-functions-solutions/src/test/kotlin/com/fpinkotlin/functions/exercise04/FunctionsTest.kt
@@ -1,6 +1,8 @@
 package com.fpinkotlin.functions.exercise04
 
 
+import com.fpinkotlin.functions.exercise04.error.square
+import com.fpinkotlin.functions.exercise04.error.triple
 import com.fpinkotlin.generators.forAll
 import io.kotlintest.properties.Gen
 import io.kotlintest.specs.StringSpec
