0205f1a85e160250c683975148cb50f1b3a000fa
pysaumont <pys@volga.fr>
2019-02-23 12:01:21 +0100

    Fixed bild

--- examples/kotlingradle/MyMultipleProject/common/build.gradle.kts
+++ examples/kotlingradle/MyMultipleProject/common/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 
 dependencies {
     compile(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1")
     testCompile("io.kotlintest:kotlintest-runner-junit5:${project.rootProject.ext["kotlintestVersion"]}")
     testRuntime("org.slf4j:slf4j-nop:${project.rootProject.ext["slf4jVersion"]}")
 //    testRuntime("ch.qos.logback:logback-classic:${project.rootProject.ext["logbackVersion"]}")
