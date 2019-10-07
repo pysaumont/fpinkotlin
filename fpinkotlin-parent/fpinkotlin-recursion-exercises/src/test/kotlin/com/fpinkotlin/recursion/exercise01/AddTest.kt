5286eeee0ec4bd10b0846743740a6a6bc31f0a1b
MrBergin <jordan.j.bergin@protonmail.com>
2019-10-07 15:54:51 +0100

    Enforce exercise 4.1 test to only test positive integers as per the book's definition.

--- fpinkotlin-parent/fpinkotlin-recursion-exercises/src/test/kotlin/com/fpinkotlin/recursion/exercise01/AddTest.kt
+++ fpinkotlin-parent/fpinkotlin-recursion-exercises/src/test/kotlin/com/fpinkotlin/recursion/exercise01/AddTest.kt
@@ -9,7 +9,7 @@ class ListTest: StringSpec() {
 
         "add" {
             forAll { a: Int, b: Int ->
-                add(a, b) == a + b
+                a <= 0 || b <= 0 || add(a, b) == a + b
             }
         }
     }
